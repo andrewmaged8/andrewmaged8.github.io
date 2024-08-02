854b9ed7b4e318765f77d02ea0d385c8a6e44e8c
Andrew Maged <dodomaged2000@gmail.com>
2024-08-02 22:15:55 +0300

    font

--- js/playImg.js
+++ js/playImg.js
@@ -1,25 +1,28 @@
-
 var btn = document.getElementById("heartTxt");
 btn.style.opacity = 0;
 var btnVal = 0;
 
-
 // Preload images for smoother transitions
 function preloadImages() {
-	for (var i = 0; i < imageArray.length; i++) {
-		var img = new Image();
-		img.src = imageArray[i];
-	}
+    for (var i = 0; i < imageArray.length; i++) {
+        var img = new Image();
+        img.src = imageArray[i];
+    }
 }
 
 window.onload = function() {
-	preloadImages();
+    preloadImages();
 };
 
 function showImage() {
     // Set the image source and text for the current index
     myImage.setAttribute("src", imageArray[imageIndex]);
     myTxt.innerHTML = txtArray[imageIndex];
+
+    // Apply styles to the text elements
+    myTxt.style.color = 'white';
+    myTxt.style.textAlign = 'center';
+    myTxt.style.display = 'block';
     
     // Wait for the image to load before proceeding to the next image and text
     myImage.onload = function() {
@@ -34,7 +37,6 @@ function showImage() {
         }, 2500); // Adjust this delay as needed
     };
 }
-
 
 function play() {
     if (t == 0) {
@@ -53,42 +55,44 @@ function play() {
     t++;
 }
 
-
 function preshowImage(){
-	document.getElementById("imgTxt").style.opacity = 0;
-	myImage.setAttribute("src", imageArray[imageIndex]);
-	myTxt.innerHTML = txtArray[imageIndex];
-	imageIndex++;
-	if(imageIndex >= len){
-		imageIndex = 0;
-	}
+    document.getElementById("imgTxt").style.opacity = 0;
+    myImage.setAttribute("src", imageArray[imageIndex]);
+    myTxt.innerHTML = txtArray[imageIndex];
+    
+    // Apply styles to the text elements
+    myTxt.style.color = 'white';
+    myTxt.style.textAlign = 'center';
+    myTxt.style.display = 'block';
+    
+    imageIndex++;
+    if(imageIndex >= len){
+        imageIndex = 0;
+    }
 }
 
 function buttonFadeIn(){
-	if(btnVal < 1){
-		btnVal += 0.025;
-		btn.style.opacity = btnVal;
-	}
-	else{
-		clearInterval(buttonInterval);
-		if(ok == 3){
-			ok += 1;
-		}
-	}
+    if(btnVal < 1){
+        btnVal += 0.025;
+        btn.style.opacity = btnVal;
+    }
+    else{
+        clearInterval(buttonInterval);
+        if(ok == 3){
+            ok += 1;
+        }
+    }
 }
 
-
-
 function event(){
+    showImageInterval = setInterval(preshowImage, 100);
 
-	showImageInterval = setInterval(preshowImage, 100);
-
-	imgInterval = setInterval(function (){
-		if(ok == 3){
-			setTimeout(function(){buttonInterval = setInterval(buttonFadeIn, 50);}, 1500);
-			clearInterval(imgInterval);
-		}
-	}, 50);
+    imgInterval = setInterval(function (){
+        if(ok == 3){
+            setTimeout(function(){buttonInterval = setInterval(buttonFadeIn, 50);}, 1500);
+            clearInterval(imgInterval);
+        }
+    }, 50);
 }
 
 var showImageInterval;
